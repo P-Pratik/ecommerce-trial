e3dc8ca317cae453984a02b3ee37dd3b21039a22
Harsh-Tayade <120238261+Harsh-Tayade@users.noreply.github.com>
2024-01-23 18:28:36 +0530

    Update item_page.php

--- ecom/item_page.php
+++ ecom/item_page.php
@@ -88,6 +88,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</body>
+    <div class="d-flex flex-column justify-content-end">
+        <?php require 'partials/_footer.php'; ?>
+    </div>
+    </body>
 </html>
 
